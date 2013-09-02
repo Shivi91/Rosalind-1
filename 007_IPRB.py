2f9bb91c09245c79bb3ffd776ea98532bea27c3e
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-01 21:35:43 -0500

    Mendel's First Law
    Solution to Rosalind Problem 007: Mendel's First Law

---
+++ 007_IPRB.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title: Mendel's First Law
+Rosalind ID: IPRB
+Rosalind #: 007
+URL: http://rosalind.info/problems/iprb/
+'''
+
+from scipy.misc import comb
+
+input_file = open('data/rosalind_iprb.txt')
+hom, het, rec = map(int, input_file.read().split())
+input_file.close()
+
+# The total number different children that can be produced by two organisms. Factor of 4 because Punit squares yield 4 potential children. 
+total = 4*comb(hom+het+rec, 2)
+
+# The number of potential children who display the recessive gene.
+totalrec = 4*comb(rec,2) + 2*rec*het + 1*comb(het,2)
+
+# Using the complementary even to find the probability of a dominant gene expression.
+print 1 - totalrec/total
