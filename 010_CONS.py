a4c47f914d9589c7c120c87e8e9760e6b69bba06
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-04 17:14:18 -0500

    Consensus and Profile
    Solution to Rosalind Problem 010: Consensus and Profile

---
+++ 010_CONS.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title: Consensus and Profile
+Rosalind ID: SUBS
+Rosalind #: 009
+URL: http://rosalind.info/problems/subs/
+'''
+
+from numpy import zeros
+from scripts import ReadFASTA
+
+# Data is in FASTA form
+dna_list = ReadFASTA('data/rosalind_cons.txt')
+
+
+# Setup an array and count into the array
+M = zeros((4,len(dna_list[0][1])), dtype = int)
+snp_dict = {'A':0, 'C':1, 'G':2, 'T':3}
+for dna in dna_list:
+    for index, snp in enumerate(dna[1]):
+        M[snp_dict[snp]][index] += 1
+
+# Determine the consensus string
+consensus = ''
+to_snp = {0:'A', 1:'C', 2:'G', 3:'T'}
+for i in range(0,len(dna_list[0][1])):
+    maxval = [-1,-1]
+    for j in range(0,4):
+        if maxval[1] < M[j][i]:
+            maxval = [j, M[j][i]]
+    consensus += to_snp[maxval[0]]
+
+
+# Format the count properly
+strs = ['A: ', 'C: ', 'G: ', 'T: ']
+for index, col in enumerate(M):
+    for val in col:
+        strs[index] += str(val)+' '
+
+# Print the output
+to_write = consensus + '\n'
+print consensus
+for out in strs:
+    print out.rstrip()
+    to_write += out.rstrip() + '\n'
+
+output_file = open('output/010_CONS.txt', 'w')
+output_file.write(to_write)
+output_file.close()
