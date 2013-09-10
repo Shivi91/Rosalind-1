847528777784f6d4f4d6911f2499887939cf652d
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-10 14:16:52 -0500

    Introduction to Random Strings
    Solution to Rosalind Problem 0028: Introduction to Random Strings

---
+++ 028_PROB.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title: Introduction to Random Strings
+Rosalind ID: PROB
+Rosalind #: 028
+URL: http://rosalind.info/problems/prob/
+'''
+
+from math import log10
+
+file1 = open('data/rosalind_prob.txt')
+dna, gc_content = file1.readlines()
+file1.close()
+
+gc_content = map(float, gc_content.split())
+
+# Counts in the number of G/C codons in index 0 and A/T codons in index 1.
+codon_count = [0, 0]
+for codon in dna:
+	if codon in ['C', 'G']:
+		codon_count[0] += 1
+	elif codon in ['A', 'T']:
+		codon_count[1] += 1
+
+
+# We use the exponent and product rules for logarithms as a shortcut. 
+# This is why we calculated the codon counts.
+gc_prob = []
+for gc_value in gc_content:
+	log_prob = codon_count[0]*log10(0.5*gc_value) + codon_count[1]*log10(0.5*(1-gc_value))
+	# Append as a string since we'll use a string join later.
+	gc_prob.append(str(log_prob))
+
+output_file = open('output/028_PROB.txt', 'w')
+output_file.write(' '.join(gc_prob))
+output_file.close()
