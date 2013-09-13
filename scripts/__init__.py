ba91c68397ee8d33c328a24fb48a424ecbdf49b3
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-13 00:35:50 -0500

    Script: Monoisotopic Mass Dictionary
    A script to translate protein to its corresponding monoisotopic mass.

---
+++ scripts/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''
+Scripts for functions common to multiple ROSALIND bioinformatics problems.
+'''
+
+from FASTA import ReadFASTA
+from RNA_to_Protein import RNA_to_Protein_Dict
+from Monoisotopic_Mass import Protein_Weight_Dict
