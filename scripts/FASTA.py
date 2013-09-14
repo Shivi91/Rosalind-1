faa240ac3435576115bacc8804264d33237f9d93
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-14 01:40:29 -0500

    Scripts: Read FASTA Info (Updated)
    Updated the script which parses and returns FASTA data.  It can now
    parse FASTA data from the web in addition to text files.  It can also
    handle a list containing websites and textfile locations.

--- scripts/FASTA.py
+++ scripts/FASTA.py
@@ -1,28 +1,52 @@
 #!/usr/bin/env python
-'''A ROSALIND bioinformatics script to extract FASTA DNA data.'''
+'''A ROSALIND bioinformatics script to extract sequence information FASTA format data.'''
 
-def ReadFASTA(fname):
-        '''Extracts DNA name and DNA sequence from the a FASTA styled file.'''
-        dna_list = []
-        with open(fname) as f:
-        	for line in f:
+import urllib
+import contextlib
 
-        		# If the line starts with '>' we've hit a new DNA strand, so append the old one and create the new one.
-        		if line[0] == '>':
-        			
-        			# Using try/except because intially there will be no current DNA strand to append.
-        			try:
-        				dna_list.append(current_dna)
-        			except UnboundLocalError:
-        				pass
-  
-        			current_dna = [line.lstrip('>').rstrip('\n'),'']
+def ReadFASTA(data_location):
+        '''Determines the data type of the FASTA format data and passes the appropriate information to be parsed.'''
+        
+        # If given a list, return fasta information from all items in the list.
+        if type(data_location) == list:
+                fasta_list =[]
+                for location in data_location:
+                        fasta_list+=ReadFASTA(location)
+                return fasta_list
 
-        		# Otherwise, append the current DNA line to the current DNA
-        		else:
-        			current_dna[1] += line.rstrip('\n')
-        	
-        	# Append the final DNA strand after reading all the lines.
-        	dna_list.append(current_dna)
-     
-        return dna_list
+
+        # Check for a text file, return fasta info from the text file.
+        if data_location[-4:] == '.txt':
+                with open(data_location) as f:
+                        return ParseFASTA(f)
+        
+        # Check for a website, return fasta info from the website.
+        elif data_location[0:4] == 'http':
+                with contextlib.closing(urllib.urlopen(data_location)) as f:
+                        return ParseFASTA(f)
+
+
+def ParseFASTA(f):
+        '''Extracts the Sequence Name and Nucleotide/Peptide Sequence from the a FASTA format file or website.'''
+        fasta_list=[]
+        for line in f:
+
+                # If the line starts with '>' we've hit a new DNA strand, so append the old one and create the new one.
+                if line[0] == '>':
+                        
+                        # Using try/except because intially there will be no current DNA strand to append.
+                        try:
+                                fasta_list.append(current_dna)
+                        except UnboundLocalError:
+                                pass
+
+                        current_dna = [line.lstrip('>').rstrip('\n'),'']
+
+                # Otherwise, append the current DNA line to the current DNA
+                else:
+                        current_dna[1] += line.rstrip('\n')
+        
+        # Append the final DNA strand after reading all the lines.
+        fasta_list.append(current_dna)
+
+        return fasta_list
