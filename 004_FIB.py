77c50184fc5a72338f5d0f95d6a3ca8addca0d74
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-16 18:12:29 -0500

    Rabbits and Recurrence Relations (updated)
    Updated to use the with statement for file handling.  This ensures that
    files will close properly, and it's more Pythonic.

--- 004_FIB.py
+++ 004_FIB.py
@@ -9,8 +9,7 @@ URL: http://rosalind.info/problems/fib/
 '''
 
 def Fib(n,k):
-    '''The total number of rabbit pairs that will be present after n months if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs'''
-    
+    '''The total number of rabbit pairs that will be present after n months if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs''' 
     if n < 1:
         return 0
     elif n == 1:
@@ -20,9 +19,7 @@ def Fib(n,k):
     else:
         return Fib(n-1,k) + k*Fib(n-2, k)
 
-
-input_file = open('data/rosalind_fib.txt')
-n,k = map(int, input_file.read().split())
-input_file.close()
+with open('data/rosalind_fib.txt') as input_data:
+	n,k = map(int, input_data.read().split())
 
 print Fib(n,k)
