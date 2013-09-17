795b7a63adba10f303c7fd86c468b3c0be46c7f7
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-16 19:33:48 -0500

    Mendel's First Law (updated)
    Updated to use the with statement for file handling.  This ensures that
    files will close properly, and it's more Pythonic.

--- 007_IPRB.py
+++ 007_IPRB.py
@@ -10,9 +10,8 @@ URL: http://rosalind.info/problems/iprb/
 
 from scipy.misc import comb
 
-input_file = open('data/rosalind_iprb.txt')
-hom, het, rec = map(int, input_file.read().split())
-input_file.close()
+with open('data/rosalind_iprb.txt') as input_data:
+	hom, het, rec = map(int, input_data.read().split())
 
 # The total number different children that can be produced by two organisms. Factor of 4 because Punit squares yield 4 potential children. 
 total = 4*comb(hom+het+rec, 2)
