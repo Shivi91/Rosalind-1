e1500a33f8ada42bc1c9ec7bed343e85b5c142b7
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-16 19:49:47 -0500

    Overlap Graphs (updated)
    Updated to use the with statement for file handling.  This ensures that
    files will close properly, and it's more Pythonic.

--- 012_GRPH.py
+++ 012_GRPH.py
@@ -13,22 +13,20 @@ from scripts import ReadFASTA
 # Data is in FASTA form
 dna_list = ReadFASTA('data/rosalind_grph.txt')
 
-output_file = open('output/012_GRPH.txt', 'w')
-line_written = False
-
-for i in range(len(dna_list)):
-    checklist = range(len(dna_list))
-    checklist.remove(i)
-    for j in checklist:
-        if  dna_list[i][1][len(dna_list[i][1])-3:len(dna_list[i][1])] == dna_list[j][1][0:3]:
-            # Display the names of the adjacent DNA strands.
-            print dna_list[i][0], dna_list[j][0]
-            # Save the adjancent strand names to an output file.
-            # No new line preceeding the first entry of output file.
-            if not line_written:
-                output_file.write(dna_list[i][0]+' '+dna_list[j][0])
-                line_written = True
-            else:
-                output_file.write('\n'+dna_list[i][0]+' '+dna_list[j][0])
-        
-output_file.close()
+# Open the output file
+with open('output/012_GRPH.txt', 'w') as output_data:
+    line_written = False
+    for i in range(len(dna_list)):
+        checklist = range(len(dna_list))
+        checklist.remove(i)
+        for j in checklist:
+            if  dna_list[i][1][len(dna_list[i][1])-3:len(dna_list[i][1])] == dna_list[j][1][0:3]:
+                # Display the names of the adjacent DNA strands.
+                print dna_list[i][0], dna_list[j][0]
+                # Save the adjancent strand names to an output file.
+                # No new line preceeding the first entry of output file.
+                if not line_written:
+                    output_data.write(dna_list[i][0]+' '+dna_list[j][0])
+                    line_written = True
+                else:
+                    output_data.write('\n'+dna_list[i][0]+' '+dna_list[j][0])
