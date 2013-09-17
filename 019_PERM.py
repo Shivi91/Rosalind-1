6354dd508622b1321b2ac96d9795ee3fb0abb294
jschendel <Jeremy.R.Schendel@gmail.com>
2013-09-16 19:58:40 -0500

    Enumerating Gene Orders (updated)
    Updated to use the with statement for file handling.  This ensures that
    files will close properly, and it's more Pythonic.

--- 019_PERM.py
+++ 019_PERM.py
@@ -17,20 +17,17 @@ def factorial(n):
 	else:
 		return n*factorial(n-1)
 
-file1 = open('data/rosalind_perm.txt')
-perm_set = range(1, int(file1.read())+1)
-file1.close()
+with open('data/rosalind_perm.txt') as input_data:
+	perm_set = range(1, int(input_data.read())+1)
 
 # Create a list containing ordered lists of all permutations.
 perm_list = map(list,list(permutations(perm_set)))
 
-output_file = open('output/019_PERM.txt', 'w')
+with open('output/019_PERM.txt', 'w') as output_data: 
+	
+	# Write the total number of permutations, n!
+	output_data.write(str(factorial(len(perm_set))))
 
-# Write the total number of permutations, n!
-output_file.write(str(factorial(len(perm_set))))
-
-# Write the permuations in the desired format.
-for permutation in perm_list:
-	output_file.write('\n'+' '.join(map(str,permutation)))
-
-output_file.close()
+	# Write the permuations in the desired format.
+	for permutation in perm_list:
+		output_data.write('\n'+' '.join(map(str,permutation)))
