36db4f31e17d3dac5a489351444042d375686d3e
jschendel <Jeremy.R.Schendel@gmail.com>
2013-10-04 16:19:04 -0500

    Independent Segregation of Chromosomes (update)
    Small modifications to speed up computations.

--- 060_INDC.py
+++ 060_INDC.py
@@ -14,16 +14,17 @@ from math import log10
 with open('data/rosalind_indc.txt') as input_data:
 	n = int(input_data.read().strip())
 
-A = [0]*2*n
-
-# Pull out a factor of 2**(-2*n) so we don't get log10(0) problems with large n.
+# Pull out a factor of 2**(-2*n) to avoid precision problems with large n.
 prob = 2**(2*n)
+
+# Initialize as the log of the factor of the probability that needs to be recombined.
+ A = [-2*n*log10(2)]*2*n
 
 for i in xrange(2*n):
 	# Recall that we pulled out a factor of 2**(-2*n).
-	prob -= comb(2*n,i, exact=True)
-	# Factor back in the 2**(-2*n) when taking the log!  
-	A[i] = -2*n*log10(2)+log10(prob)
+	prob -= comb(2*n, i, exact=True)
+	# Covert to log and add in factor 2**(-2*n) using log rules.  
+	A[i] += log10(prob)
 
 with open('output/060_INDC.txt', 'w') as output_data:
 	output_data.write(' '.join(map(str,A)))
