97a1060de045f58f568c216170720ce11243027c
jschendel <Jeremy.R.Schendel@gmail.com>
2013-10-13 22:23:45 -0500

    Frequent Words with Mismatches Problem (updated)
    Cleaned up the code.  Same functionality as before.

--- Textbook_01G.py
+++ Textbook_01G.py
@@ -15,49 +15,34 @@ def MismatchList(kmer, d):
 	'''Returns a list of all k-mers that mismatch a given k-mer by at most d characters.'''
 	kmer_mismatches = [kmer]
 	for i in xrange(1,d+1):
-		for combo in combinations(range(len(kmer)),i):
-			kmer_mismatches += CreateMismatches([[kmer, list(combo)]])
-
+		# Each combination gives the indicies we want to mismatch.
+		kmer_mismatches += CreateMismatches([[kmer, list(combo)] for combo in combinations(range(len(kmer)),i)])
 	return kmer_mismatches
 
 
 def CreateMismatches(swap_list):
-	'''Generates mismatching k-mers by replacing the character a given index.'''
+	'''Generates k-mer mismatches by replacing the characters at given indicies with mismatching characters.'''
 	nucleotides = 'ACGT'
-	
+	mismatch_list = []
+	# Swap the i-th character of string with the character ch.
+	swap = lambda string, ch, i: string[:index]+ch+string[index+1:]
+
 	# If we have more than one index left to mismatch, repeat the process.
 	if len(swap_list[0][1]) > 1:
-		mismatch_list = []
 		for kmer, indicies in swap_list:
 			index = indicies[0]
-			indicies = indicies[1:]
-			for nuc in [nuc for nuc in nucleotides if nuc != kmer[index]]:
-				temp = list(kmer)
-				temp[index] = nuc
-				mismatch_list.append([''.join(temp), indicies])
+			for nuc in [nucleotide for nucleotide in nucleotides if nucleotide != kmer[index]]:
+				mismatch_list.append([swap(kmer, nuc, index), indicies[1:]])
+		
 		return CreateMismatches(mismatch_list)
 	
-	# If on the final mismatch, return the list of k-mers.
+	# Otherwise, on the final mismatch return the list of k-mers.
 	else:
-		mismatch_list = []
 		for kmer, [index] in swap_list:
 			for nuc in [nuc for nuc in nucleotides if nuc != kmer[index]]:
-				temp = list(kmer)
-				temp[index] = nuc
-				mismatch_list.append(''.join(temp))
+				mismatch_list.append(swap(kmer, nuc, index))
+		
 		return mismatch_list
-
-
-def KmerMismatchDict(dna, k, d):
-	'''Returns a dictionary listing the number of times a k-mer appears in a dna strand, up to d mismatches.'''
-	kmer_dict = dict()
-	for i in xrange(len(dna)-k+1):
-		for kmer in MismatchList(dna[i:i+k], d):
-			if kmer in kmer_dict:
-				kmer_dict[kmer] += 1
-			else:
-				kmer_dict[kmer] = 1
-	return kmer_dict
 
 
 if __name__ == '__main__':
@@ -65,11 +50,18 @@ if __name__ == '__main__':
 	with open('data/textbook/rosalind_1g.txt') as input_data:
 		dna, [k, d] = [line.strip() if index == 0 else map(int, line.strip().split()) for index, line in enumerate(input_data.readlines())]
 
-	kmer_mismatch_dict = KmerMismatchDict(dna, k, d)
+	# Count the occurence of each k-mer with up to d mismatches in a dictionary.
+	mismatch_dict = {}
+	for i in xrange(len(dna)-k+1):
+		for kmer in MismatchList(dna[i:i+k], d):
+			if kmer in mismatch_dict:
+				mismatch_dict[kmer] += 1
+			else:
+				mismatch_dict[kmer] = 1
 
-	# Computing the maximum value is time consuming to repeat, so only do it once!
-	max_val = max(kmer_mismatch_dict.values())
-	kmers = [item[0] for item in kmer_mismatch_dict.items() if item[1] == max_val]
+	# Computing the maximum value is somewhat time consuming to repeat, so only do it once!
+	max_val = max(mismatch_dict.values())
+	kmers = [item[0] for item in mismatch_dict.items() if item[1] == max_val]
 
 	print ' '.join(kmers)
 	with open('output/textbook/Textbook_01G.txt', 'w') as output_data:
