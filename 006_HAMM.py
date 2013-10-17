83540b01fe2b8cb642577b9bd411302d9cffeb25
jschendel <Jeremy.R.Schendel@gmail.com>
2013-10-17 15:54:11 -0500

    Counting Point Mutations (update)
    Updated to utilize the new hamming distance script.

--- 006_HAMM.py
+++ 006_HAMM.py
@@ -8,13 +8,12 @@ Rosalind #: 006
 URL: http://rosalind.info/problems/hamm/
 '''
 
+from scripts import HammingDistance
+
 with open('data/rosalind_hamm.txt') as input_data:
 	s, t = [line.rstrip('\n') for line in input_data.readlines()]
 
-count = 0
-for index, dna in enumerate(s):
-    if dna != t[index]:
-        count += 1
+count = HammingDistance(s,t)
 
 print count
 with open('output/006_HAMM.txt', 'w') as output_data:
