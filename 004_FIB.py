97ca088e5e27cdad0a2c8cabac73dc3ca0ef00ba
jschendel <Jeremy.R.Schendel@gmail.com>
2013-10-17 00:45:38 -0500

    Rabbits and Recurrence Relations (update)
    Updated to write to an output file.

--- 004_FIB.py
+++ 004_FIB.py
@@ -9,7 +9,10 @@ URL: http://rosalind.info/problems/fib/
 '''
 
 def Fib(n,k):
-    '''The total number of rabbit pairs that will be present after n months if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs''' 
+    '''The total number of rabbit pairs that will be present 
+    after n months if we begin with 1 pair and in each generation, 
+    every pair of reproduction-age rabbits produces a litter of k rabbit pairs
+    ''' 
     if n < 1:
         return 0
     elif n == 1:
@@ -22,4 +25,7 @@ def Fib(n,k):
 with open('data/rosalind_fib.txt') as input_data:
 	n,k = map(int, input_data.read().split())
 
-print Fib(n,k)
+rabbits = str(Fib(n,k))
+print rabbits
+with open('output/004_FIB.txt', 'w') as output_data:
+    output_data.write(rabbits)
