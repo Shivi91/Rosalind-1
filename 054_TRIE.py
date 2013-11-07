de066e5bd003d8dc73d620974537d2c9fcc8c5a1
jschendel <Jeremy.R.Schendel@gmail.com>
2013-11-07 17:54:08 -0600

    Introduction to Pattern Matching (clean up)
    Removed duplicated code and utilized the Trie class from the Data
    Structures script.  Also, simplified a multiline for statement into a
    one line list comprehension.  Closes #3.

--- 054_TRIE.py
+++ 054_TRIE.py
@@ -8,78 +8,15 @@ Rosalind #: 054
 URL: http://rosalind.info/problems/trie/
 '''
 
-class Trie(object):
-    '''Constructs a Trie'''
-    def __init__(self):
-        self.nodes = [[Node('', 1)]]
-        self.edges = []
+from scripts import Trie
 
-    def addWord(self, word):
-    	if type(word) == list:
-    		for w in word:
-    			self.addWord(w)
-    	else:
-	        parent = self.findParent(word)
-	        for i in range(len(parent.prefix), len(word)):
-	            new_node = Node(word[:i+1], self.nodeCount()+1)
-	            self.edges.append(Edge(word[i], parent.number, self.nodeCount()+1))
-	            self.insertNode(new_node)
-	            parent = new_node
+with open('data/rosalind_trie.txt') as input_data:
+	dna = [line.strip() for line in input_data.readlines()]
 
-    def insertNode(self, node):
-        if node.depth > self.depth():
-            self.nodes.append([node])
-        else:
-            self.nodes[node.depth].append(node)
+# The heavy lifting is done by the Trie class in the Data Structures script.
+myTrie = Trie(dna)
+adjacency_list = [edge.getInfo() for edge in myTrie.edges]
 
-    def depth(self):
-        '''Returns the depth of the trie.'''
-        return len(self.nodes) - 1
-
-    def nodeCount(self):
-        count = 0
-        for trie_depth in self.nodes:
-            count += len(trie_depth)
-        return count
-
-    def findParent(self, word):
-        '''Return the parent node of the word to be inserted.'''
-        for i in range(min(len(word), self.depth()), 0,-1):
-            for node in self.nodes[i]:
-                if word[:i] == node.prefix:
-                    return node
-
-        return self.nodes[0][0]
-
-class Node(object):
-    '''Trie Node'''
-    def __init__(self, prefix, number):
-        self.prefix = prefix
-        self.number = number
-        self.depth = len(prefix)
-
-class Edge(object):
-    '''Trie Edge'''
-    def __init__(self, letter, par_node, chi_node):
-        self.letter = letter
-        self.parent_node = par_node
-        self.child_node = chi_node
-
-    def getInfo(self):
-    	return ' '.join(map(str,[self.parent_node,self.child_node,self.letter]))
-
-if __name__ == '__main__':
-	
-	with open('data/rosalind_trie.txt') as input_data:
-		dna = [line.strip() for line in input_data.readlines()]
-
-	myTrie = Trie()
-	myTrie.addWord(dna)
-
-	adjacency_list = []
-	for edge in myTrie.edges:
-		adjacency_list.append(edge.getInfo())
-
-	print '\n'.join(adjacency_list)
-	with open('output/054_TRIE.txt', 'w') as output_file:
-		output_file.write('\n'.join(adjacency_list))
+print '\n'.join(adjacency_list)
+with open('output/054_TRIE.txt', 'w') as output_file:
+	output_file.write('\n'.join(adjacency_list))
