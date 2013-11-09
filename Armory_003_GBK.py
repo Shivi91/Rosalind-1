79392538985aa18a0240db081eb9f427b615f49c
jschendel <Jeremy.R.Schendel@gmail.com>
2013-11-08 20:38:40 -0600

    GenBank Introduction (fix typo)
    Fixed a missing ']' in the search term.  Functionality remains the same.

--- Armory_003_GBK.py
+++ Armory_003_GBK.py
@@ -15,7 +15,7 @@ with open('data/armory/rosalind_gbk.txt') as input_data:
 	genus, start_date, end_date = [line.strip() for line in input_data.readlines()]
 
 Entrez.email = 'jschendel@users.noreply.github.com'
-handle = Entrez.esearch(db='nucleotide', term=genus+'[ORGN', mindate=start_date, maxdate=end_date, datetype='pdat')
+handle = Entrez.esearch(db='nucleotide', term=genus+'[ORGN]', mindate=start_date, maxdate=end_date, datetype='pdat')
 record = Entrez.read(handle)
 
 print record['Count']
