8bea8b65762d4fd40ac60484ed99317a6fd44253
jschendel <Jeremy.R.Schendel@gmail.com>
2013-11-17 14:17:21 -0600

    Finding a Shared Spliced Motif (updated)
    Rewrote the solution as a function, so it could be accessed in a later
    problem.  Solution method remains the same.

--- 038_LCSQ.py
+++ 038_LCSQ.py
@@ -11,31 +11,40 @@ URL: http://rosalind.info/problems/lcsq/
 from numpy import zeros
 from scripts import ReadFASTA
 
-# Read FASTA DNA sequences.
-dna1, dna2 = [fasta[1] for fasta in ReadFASTA('data/rosalind_lcsq.txt')]
+def longest_common_subsequence(dna1, dna2):
+	'''Returns the longest longest common subsequence of dna1 and dna2.'''
+	# Compute each entry of M.
+	M = zeros((len(dna1)+1,len(dna2)+1))
+	for i in xrange(len(dna1)):
+		for j in xrange(len(dna2)):
+			if dna1[i] == dna2[j]:
+				M[i+1][j+1] = M[i][j]+1
+			else:
+				M[i+1][j+1] = max(M[i+1][j],M[i][j+1])
 
-# Compute each entry of M.
-M = zeros((len(dna1)+1,len(dna2)+1))
-for i in xrange(len(dna1)):
-	for j in xrange(len(dna2)):
-		if dna1[i] == dna2[j]:
-			M[i+1][j+1] = M[i][j]+1
+	# Recover a maximum substring.
+	longest_sseq = ''
+	i,j = len(dna1), len(dna2)
+	while i*j != 0:
+		if M[i][j] == M[i-1][j]:
+			i -= 1
+		elif M[i][j] == M[i][j-1]:
+			j -= 1
 		else:
-			M[i+1][j+1] = max(M[i+1][j],M[i][j+1])
+			longest_sseq = dna1[i-1] + longest_sseq
+			i -= 1
+			j -= 1
 
-# Recover a maximum substring.
-longest_sseq = ''
-i,j = len(dna1), len(dna2)
-while i*j != 0:
-	if M[i][j] == M[i-1][j]:
-		i -= 1
-	elif M[i][j] == M[i][j-1]:
-		j -= 1
-	else:
-		longest_sseq = dna1[i-1] + longest_sseq
-		i -= 1
-		j -= 1
+	return longest_sseq
 
-print longest_sseq
-with open('output/038_LCSQ.txt', 'w') as output_file:
-	output_file.write(longest_sseq)
+if __name__ == '__main__':
+	# Read FASTA DNA sequences.
+	dna1, dna2 = [fasta[1] for fasta in ReadFASTA('data/rosalind_lcsq.txt')]
+	
+	# Solve the problem
+	lcsq = longest_common_subsequence(dna1,dna2)
+
+	# Print and save the output.
+	print lcsq
+	with open('output/038_LCSQ.txt', 'w') as output_file:
+		output_file.write(lcsq)
